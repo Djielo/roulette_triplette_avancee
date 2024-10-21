3cfc0a371628c620f27db8ba6eea656abc7f3946
Djielo <business.jlo68@gmail.com>
2024-10-21 16:44:35 +0200

    build avec pyinstaller

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from gui.roulette_gui import RouletteGUI
 
 def main():
     root = tk.Tk()
-    root.geometry("1000x800")  # Ajusté pour accommoder la nouvelle interface
+    
+    # Récupérer la taille de l'écran
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    
+    # Ajuster la fenêtre à 80% de la taille de l'écran
+    root.geometry(f"{int(screen_width * 0.6)}x{int(screen_height * 0.8)}")
+    
+    # S'assurer que la fenêtre se redimensionne
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    
+    # Créer l'interface avec la classe RouletteGUI
     gui = RouletteGUI(root)
+    
     root.mainloop()
 
 if __name__ == "__main__":
